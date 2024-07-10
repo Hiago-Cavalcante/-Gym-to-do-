fd312c4bdb087f208656ad7eee61e5ce328a0076
Hiago Dev <hiagocavalcante03@gmail.com>
2024-07-10 16:32:18 -0300

    yep

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 'use strict';
 
+const modal = document.getElementById('modal-window-task');
+
 const openFormModal = document
   .querySelector('.btn-add-task')
   .addEventListener('click', function () {
-    const modal = document.getElementById('modal-window-task');
     modal.classList.add('openModal');
-
-    modal.addEventListener('click', function (e) {
-      if (
-        e.target.id == 'btn-close-modal' ||
-        e.target.id == 'modal-window-task'
-      )
-        modal.classList.remove('openModal');
-
-      const inputs = modal.querySelectorAll(
-        'input[type="text"], input[type="number"], textarea'
-      );
-      inputs.forEach(input => (input.value = ''));
-    });
   });
 
 const listContainer = document.getElementById('list-container');
+
+
+const closeFormModal = modal.addEventListener('click', function (e) {
+  if (
+    e.target.id == 'btn-close-modal' ||
+    e.target.id == 'modal-window-task'
+  ) 
+    modal.classList.remove('openModal');
+
+  const inputs = modal.querySelectorAll(
+    'input[type="text"], input[type="number"], textarea'
+  );
+  inputs.forEach(input => (input.value = ''));
+});
